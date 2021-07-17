38e543feb9cb839ef95c93d81b20c55cec197872
wadakatu <wadakatukoyo330@gmail.com>
2021-07-18 01:57:36 +0900

    fontAwesome追加・いいねボタン作成・配置

--- koyolympus/resources/js/components/PhotoModalComponent.vue
+++ koyolympus/resources/js/components/PhotoModalComponent.vue
@@ -6,19 +6,24 @@
                     <img :src="val.url" alt="This photo taken by Koyo Isono.">
                 </div>
                 <div id="modal-content-bottom">
-                    <button class="good">いいね！</button>
-                    <button class="close" @click="$emit('close')">閉じる</button>
+                    <i class="like-heart" v-on:click.stop="close()"></i>
+                    <span class="liked">liked!</span>
                 </div>
             </div>
         </div>
     </transition>
-</template>s
+</template>
 
 <script>
 export default {
     name: "PhotoModalComponent.vue",
-    props: ['val']
+    props: ['val'],
 }
+$(function () {
+    $("i").click(function () {
+        $("i,span").toggleClass("press", 1000);
+    });
+});
 </script>
 
 <style scoped>
@@ -44,7 +49,7 @@ export default {
 #modal-content {
     z-index: 2;
     width: 80%;
-    height: 55vh;
+    height: 50vh;
     padding: 1em;
     background-color: rgba(0, 0, 0, 0.5);
 }
@@ -58,7 +63,7 @@ export default {
 
 #modal-content-bottom {
     width: 80vw;
-    margin-top: 5vh;
+    margin-top: 3vh;
     text-align: center;
     /*display: inline-block;*/
     /*width: 15vw;*/
@@ -80,9 +85,75 @@ img {
     pointer-events: none;
 }
 
-.close {
-    /*top: 0;*/
-    /*left: 25vw;*/
+.like-heart {
+    cursor: pointer;
+    padding: 10px 12px 8px;
+    background: #fff;
+    border-radius: 50%;
+    display: inline-block;
+    margin: 0 0 15px;
+    color: #aaa;
+    transition: .2s;
+}
+
+.like-heart:hover {
+    color: #666;
+}
+
+.like-heart:before {
+    font-family: fontawesome;
+    content: '\f004';
+    font-style: normal;
+}
+
+.liked {
+    position: absolute;
+    bottom: 100px;
+    left: 0;
+    right: 0;
+    visibility: hidden;
+    transition: 1s;
+    z-index: -2;
+    font-size: 5px;
+    color: transparent;
+    font-weight: 400;
+}
+
+.like-heart.press {
+    animation: size .6s;
+    color: #e23b3b;
+}
+
+.liked.press {
+    bottom: 180px;
+    font-size: 20px;
+    visibility: visible;
+    animation: fade 2s;
+}
+
+@keyframes fade {
+    0% {
+        color: #transparent;
+    }
+    50% {
+        color: #e23b3b;
+    }
+    100% {
+        color: #transparent;
+    }
+}
+
+@keyframes size {
+    0% {
+        padding: 10px 12px 8px;
+    }
+    50% {
+        padding: 14px 16px 12px;
+        margin-top: -4px;
+    }
+    100% {
+        padding: 10px 12px 8px;
+    }
 }
 
 </style>

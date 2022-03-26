f62d916d980cb6e89d6fd92b03ba71aaed2987da
wadakatu <wadakatukoyo330@gmail.com>
2022-03-26 11:03:13 +0900

    tests/Unit/Jobs配下をPSR12準拠に修正

--- koyolympus/tests/Unit/Jobs/AggregateWeeklyLikeJobTest.php
+++ koyolympus/tests/Unit/Jobs/AggregateWeeklyLikeJobTest.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Tests\Unit\Jobs;
@@ -56,7 +57,7 @@ class AggregateWeeklyLikeJobTest extends TestCase
     /**
      * @test
      */
-    public function failed_exception()
+    public function failedException()
     {
         $throwable = new Exception('例外です！');
 
@@ -79,7 +80,7 @@ class AggregateWeeklyLikeJobTest extends TestCase
     /**
      * @test
      */
-    public function failed_error()
+    public function failedError()
     {
         $throwable = new Error('例外です！');
 

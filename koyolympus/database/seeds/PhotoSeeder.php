108f1ab32152eebb9bbe819375c3e1d480185596
wadakatu <wadakatukoyo330@gmail.com>
2022-04-08 23:32:58 +0900

    Seeder完成・プログレスバー追加

--- koyolympus/database/seeds/PhotoSeeder.php
+++ koyolympus/database/seeds/PhotoSeeder.php
@@ -8,23 +8,47 @@ use Illuminate\Http\UploadedFile;
 
 class PhotoSeeder extends Seeder
 {
-    private $number = 500;
+    private $number = 300;
 
     /**
      * @return void
+     * @throws Exception
      */
     public function run()
     {
-        $file = UploadedFile::fake()->image('test.jpeg', 5184, 3888)->size('2000');
-        DB::beginTransaction();
-        try {
-            for ($i = 1; $i <= $this->number; $i++) {
-                $index = (6 < $i % 10 || $i % 10 === 0) ? 1 : $i % 10;
-                $fileUrl = config("const.PHOTO.GENRE_FILE_URL.$index");
+        $this->command->getOutput()->progressStart($this->number);
+        for ($i = 1; $i <= $this->number; $i++) {
+            $index = (6 < $i % 10 || $i % 10 === 0) ? 1 : $i % 10;
+            $fileUrl = config("const.PHOTO.GENRE_FILE_URL.$index");
+
+            $photo = factory(Photo::class)->make();
+            $fileName = $photo->id . "-test.jpeg";
+            $photo->file_path = $fileUrl . '/' . $fileName;
+
+            DB::beginTransaction();
+            try {
+                $photo->save();
+                DB::commit();
+                $this->putPhotoToS3($fileUrl, $fileName);
+            } catch (Throwable $e) {
+                DB::rollBack();
+                $this->deletePhotoFromS3($fileUrl, $fileName);
+                continue;
+            }finally{
+                $this->command->getOutput()->progressAdvance();
             }
-            DB::commit();
-        } catch (Throwable $e) {
-            DB::rollBack();
         }
+        $this->command->getOutput()->progressFinish();
+    }
+
+    private function putPhotoToS3(string $fileUrl, string $fileName)
+    {
+        $file = new UploadedFile(public_path('/images/P8182102.jpeg'), $fileName);
+        Storage::disk('s3')->putFileAs($fileUrl, $file, $fileName, 'public');
+    }
+
+    private function deletePhotoFromS3(string $fileUrl, string $fileName)
+    {
+        Storage::disk('s3')->delete($fileUrl . '/' . $fileName);
     }
 }

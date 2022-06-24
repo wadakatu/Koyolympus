6da2bf39989cbc47b76ce5bded942e08cb627ea4
wadakatu <wadakatukoyo330@gmail.com>
2022-06-25 00:02:32 +0900

    シェルスクリプト更新

--- .circleci/remote.sh
+++ .circleci/remote.sh
@@ -2,7 +2,18 @@
 set -ex
 
 export TAG=$1;
+export SUCCESS=200
 
 cd /var/www/koyolympus/koyolympus
-ls -a
-echo $TAG
+git checkout . && git fetch && git checkout $TAG
+composer install
+npm install && npm run prod
+systemctl reload nginx && STATUS=`curl -LI https://koyolympus.gallery -o /dev/null -w '%{http_code}\n' -s`
+
+if [ $STATUS -ne $SUCCESS ]; then
+  exit 1;
+fi
+
+
+
+

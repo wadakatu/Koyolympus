536c35a8f5f9ee0b86be84dee77e2538eafd53d0
wadakatu <wadakatukoyo330@gmail.com>
2022-06-27 19:53:08 +0900

    artisan up & down追加

--- .circleci/remote.sh
+++ .circleci/remote.sh
@@ -4,11 +4,11 @@ set -ex
 export TAG=$1;
 export SUCCESS=200
 
-cd /var/www/koyolympus/koyolympus
+cd /var/www/koyolympus/koyolympus && php artisan down
 git checkout . && git fetch && git checkout $TAG
 composer install
 npm install && npm run prod
-systemctl reload nginx && STATUS=`curl -LI https://koyolympus.gallery -o /dev/null -w '%{http_code}\n' -s`
+systemctl reload nginx && php artisan up && STATUS=`curl -LI https://koyolympus.gallery -o /dev/null -w '%{http_code}\n' -s`
 
 if [ $STATUS -ne $SUCCESS ]; then
   exit 1;

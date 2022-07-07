62406ade4be650a5698469929c94e10445391acd
wadakatu <wadakatukoyo330@gmail.com>
2022-07-07 22:41:40 +0900

    add sudo

--- .circleci/remote.sh
+++ .circleci/remote.sh
@@ -9,7 +9,7 @@ cd /var/www/koyolympus/koyolympus && php artisan down
 git checkout . && git fetch && git checkout $TAG
 composer install
 npm install && npm run prod
-systemctl reload nginx && php artisan up && STATUS=`curl -LI https://koyolympus.gallery -o /dev/null -w '%{http_code}\n' -s`
+sudo systemctl reload nginx && php artisan up && STATUS=`curl -LI https://koyolympus.gallery -o /dev/null -w '%{http_code}\n' -s`
 
 if [ $STATUS -ne $SUCCESS ]; then
   exit 1;

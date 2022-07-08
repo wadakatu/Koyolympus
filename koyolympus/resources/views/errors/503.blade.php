06546b4ebb94824deb658abd945a4909d48b4fb2
wadakatu <wadakatukoyo330@gmail.com>
2022-07-08 21:45:50 +0900

    add urls on maintenance footer

--- koyolympus/resources/views/errors/503.blade.php
+++ koyolympus/resources/views/errors/503.blade.php
@@ -19,10 +19,10 @@
     </body>
     <footer class="social-container" ontouchstart="">
         <ul class="social-icons">
-            <li><a href="#"><i class="fa fa-github"></i></a></li>
-            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="https://github.com/wadakatu"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://twitter.com/koyolympus"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://m.facebook.com/people/Koyo-Isono/100006224742543"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="https://www.instagram.com/wadakatu1234/?hl=ja"><i class="fa fa-instagram"></i></a></li>
         </ul>
     </footer>
 @endsection
